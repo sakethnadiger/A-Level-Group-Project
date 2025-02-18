20ac9a8b6d8027ad219933fb9a6b3199ed7cdc24
ToastyEggy73 <Difonzol76@gmail.com>
2025-02-18 15:10:03 +0000

    I made the fartmachine.

--- main.py
+++ main.py
@@ -2,3 +2,12 @@ print("This code is from a branch")
 import pygame
 
 print("Hello World")
+
+working = True
+fartmachine = working
+
+if fartmachine == working:
+    print("fart")
+
+else:
+    pass
