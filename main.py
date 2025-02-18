8bdafd7e55ba268c1fc8ce125e0cf66494dae6d6
Harvey.McDonald <harveyfmcd@icloud.com>
2025-02-18 15:26:10 +0000

    yash

--- main.py
+++ main.py
@@ -1,13 +1,2 @@
-print("This code is from a branch")
-import pygame
-
-print("Hello World")
-
-working = True
-fartmachine = working
-
-if fartmachine == working:
-    print("fart")
-
-else:
-    pass
+while True:
+    print("Yash is smelly")
