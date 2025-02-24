eb77d5362b0850c7329c7d7bb476a6e3432dd9f9
yashrampersad <yash.rampersad.2007@gmail.com>
2025-02-24 12:01:48 +0000

    .

--- pygame_experiment.py
+++ pygame_experiment.py
@@ -1,13 +1,17 @@
 import pygame
 
 
-pygame.init()
+pygame.init() # creates an object of the pygame class to make our window
 
-pygame.display.set_caption('Quick Start')
-window_surface = pygame.display.set_mode((800, 600))
+pygame.display.set_caption('Quick Start') # sets the caption at the top of the window
+window_surface = pygame.display.set_mode((800, 600)) # sets the size of the window. theres some other ways to do fullscreen stc but we wont worry abt that now
 
-background = pygame.Surface((800, 600))
+background = pygame.Surface((800, 600)) # creating a background object o make a black background
 background.fill(pygame.Color('#000000'))
+
+
+# IN PYGAME YOU ALWAYS USE THIS WHILE LOOP STRUCTURE, THIS IS THE CORE SYSTEM OF PYGAME.
+# it is what allows us to constantly check for things like clicks and button presses each frame.
 
 is_running = True
 
