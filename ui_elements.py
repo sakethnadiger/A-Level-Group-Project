3e306b494694176beb32349fd1dcb36b58499da3
sakethnadiger <saketh.nadiger@gmail.com>
2025-02-26 20:00:56 +0000

    relative positioning and centred coordinates

--- ui_elements.py
+++ ui_elements.py
@@ -8,6 +8,17 @@ import pygame
 
 
 
+#NOW USING A COORDINATE SYSTEM WITH 0,0 AT THE CENTRE OF SCREEN AND INPUT COORDINATES RELATIVE TO CENTRE. E.G. 0.5 IS HALFWAY UP TO THE TOP
+#INPUT WILL BE A NUMBER x OR y, WITH |x| < 1 AND |y| < 1
+def convert_coords(input_x, input_y):
+    width, height = pygame.display.get_window_size()
+    screen_centre_x = width // 2
+    screen_centre_y = height // 2
+    true_x = int(screen_centre_x * (1 + input_x))
+    true_y = int(screen_centre_y * (1 - input_y))
+    return true_x, true_y
+
+
 class Label():
     def __init__(self, width, height, colour, text, text_size, text_colour):
         self.width = width
@@ -25,8 +36,12 @@ class Label():
     # method that draws the label to the screen. take parameters: screen to be drawn on, position x, position y
     #CHANGE - THE CENTRE OF THE ELEMENTS IS NOW WHERE THE THEY WILL BE PLACED, NOT THE TOP LEFT
     def draw(self, surface, x, y):
-        surface.blit(self.box, (x - self.width/2, y - self.width/2))
-        surface.blit(self.img, (x + (self.width//2) - self.img.get_width()//2, y + (self.height//2) - self.img.get_height()//2))
+        new_x = convert_coords(x, y)[0]
+        new_y = convert_coords(x, y)[1]
+        centred_x = new_x - self.width//2
+        centred_y = new_y - self.width//2
+        surface.blit(self.box, (centred_x, centred_y))
+        surface.blit(self.img, (centred_x + (self.width//2) - self.img.get_width()//2, centred_y + (self.height//2) - self.img.get_height()//2))
 
 #Inherited buton from label.
 class Button(Label):
@@ -36,10 +51,14 @@ class Button(Label):
 
     # self.hitbox is a rect that contains the clickable area.
     def draw(self, surface, x, y):
-        surface.blit(self.box, (x, y))
-        surface.blit(self.img, (x + (self.width//2) - self.img.get_width()//2, y + (self.height//2) - self.img.get_height()//2))
+        new_x = convert_coords(x, y)[0]
+        new_y = convert_coords(x, y)[1]
+        centred_x = new_x - self.width//2
+        centred_y = new_y - self.width//2
+        surface.blit(self.box, (centred_x, centred_y))
+        surface.blit(self.img, (centred_x + (self.width//2) - self.img.get_width()//2, centred_y + (self.height//2) - self.img.get_height()//2))
         self.hitbox = self.box.get_rect()
-        self.hitbox.x, self.hitbox.y = x, y
+        self.hitbox.x, self.hitbox.y = centred_x, centred_y
         
 
     # checks if the button is clicked. The button can only be clicked once and must be reset with the reset function. the function parameter takes the function that should be ran when clicked/.
