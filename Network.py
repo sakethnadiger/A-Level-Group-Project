6546bce70f12343fa1096dec5913a09837ab1d5f
yashrampersad <yash.rampersad.2007@gmail.com>
2025-03-03 20:53:43 +0000

    .

--- Network.py
+++ Network.py
@@ -1,40 +1,11 @@
-# import socket
-
-# # Creating a class to be reused in the future
-
-# class Network:
-#     def __init__(self):
-#         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#         self.server = "127.0.0.1" # This is this server address, not the client, it will stay the same.
-#         self.port = 5555
-#         self.addr = (self.server, self.port)
-#         self.pos = self.connect()
-
-#     def getPos(self):
-#         return self.pos
-
-#     def connect(self):
-#         try:
-#             self.client.connect(self.addr) # This will try to connect to the server.
-#             return self.client.recv(2048).decode() # Recieves 2048 bits from the server and decodes it.
-#         except:
-#             pass
-    
-#     def send(self, data):
-#         try:
-#             self.client.send(str.encode(data))
-#             return self.client.recv(2048).decode()
-#         except socket.error as e:
-#             print(e)
-
-# # n = Network() # Testing variable.
-# # print(n.send("Hello"))
-# # print(n.send("Working"))
+# the purpose of this script is to create a class that can connect the client to the server and take client requests to send to the server.
+# This is basically like the waiter at a restaurant.
 import socket
 
 
 class Network:
     def __init__(self):
+        # ensures that the network connects to the right class
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server = socket.gethostbyname(socket.gethostname())
         self.port = 5555
