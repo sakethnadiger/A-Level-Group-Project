0ef9eec92cce972a3a11b4ecac2d67a8b8b41a05
Harvey.McDonald <harveyfmcd@icloud.com>
2025-03-10 12:15:27 +0000

    .

--- Network.py
+++ Network.py
@@ -4,8 +4,8 @@ import socket
 class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server = "10.2.64.51"
-        #self.server = socket.gethostbyname(socket.gethostname())
+        # self.server = "10.2.64.51"
+        self.server = socket.gethostbyname(socket.gethostname())
         self.port = 5555
         self.addr = (self.server, self.port)
         self.pos = self.connect()
