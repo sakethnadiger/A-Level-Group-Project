ff059228fee2997a0ff01a396029ab5739f4c54b
ToastyEggy73 <difonzol76@gmail.com>
2025-03-14 09:26:01 +0000

    I FORGOR SOMETHING

--- MainScreen.py
+++ MainScreen.py
@@ -47,7 +47,7 @@ while True:
     if startbutton.is_clicked(event, startchatting):
         chatting = True
         import chat_screen
-    
+        pygame.quit()    
 
     pygame.display.update()
     clock.tick(60)
