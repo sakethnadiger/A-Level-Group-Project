acacf9e4512249461f03fca944aefca5c83e4d0c
yashrampersad <yash.rampersad.2007@gmail.com>
2025-03-19 09:21:22 +0000

    .

--- Network.py
+++ Network.py
@@ -4,8 +4,8 @@ import socket
 class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        # self.server = "192.168.0.173"
-        self.server = socket.gethostbyname(socket.gethostname())
+        self.server = "172.20.10.4"
+        # self.server = socket.gethostbyname(socket.gethostname())
         self.port = 5555
         self.addr = (self.server, self.port)
         self.id = self.connect()
@@ -18,8 +18,7 @@ class Network:
             self.client.connect(self.addr)
             return self.client.recv(2048).decode()
         except:
-            pass
-            # print("error: not able to connect the network to the server")
+            print("error: not able to connect the network to the server")
 
     def send(self, data):
         try:
--- Server.py
+++ Server.py
@@ -5,8 +5,8 @@ from _thread import *
 import sys
 
 # find the IP address of the server by taking the computer that the server runs on and open cmd > type "ipconfig" > copy the IPV4 Address.
-# server = "192.168.0.173"  # the ip address goes here
-server = socket.gethostbyname(socket.gethostname())
+server = "172.20.10.4"  # the ip address goes here
+# server = socket.gethostbyname(socket.gethostname())
 port = 5555
 
 
@@ -23,36 +23,37 @@ except socket.error as e:
 s.listen()
 print("Waiting for a connection, Server Started")
 
-to_send = []
+to_send = [[],[]]
 
 def threaded_client(conn, user):
     conn.send(str.encode("Connected"))
     reply = ""
+    print(user)
     while True:
         # try:
-            data = conn.recv(2048)
-            msg = data.decode("utf-8")
-            if not data:
-                print("Disconnected")
-                break
-            elif msg != ".":
-                print("Received: ", msg)
-                to_send[user].append(msg)
-            if user == 0:
-                if len(to_send[1]) == 0:
-                    reply = "."
-                else:
-                    reply = to_send[1][0]
-                    to_send[1].pop(-1)
-                    print("Sending : ", reply)
+        data = conn.recv(2048)
+        msg = data.decode("utf-8")
+        if not data:
+            print("Disconnected")
+            break
+        elif msg != ".":
+            print("Received: ", msg)
+            to_send[user].append(msg)
+        if user == 0:
+            if len(to_send[1]) == 0:
+                reply = "."
             else:
-                if len(to_send[0]) == 0:
-                    reply = "."
-                else:
-                    reply = to_send[0][0]
-                    to_send[0].pop(-1)
-                    print("Sending : ", reply)
-            conn.sendall(str.encode(reply))
+                reply = to_send[1][0]
+                to_send[1].pop(-1)
+                print("Sending : ", reply)
+        else:
+            if len(to_send[0]) == 0:
+                reply = "."
+            else:
+                reply = to_send[0][0]
+                to_send[0].pop(-1)
+                print("Sending : ", reply)
+        conn.sendall(str.encode(reply))
         # except:
         #     print("error occured in threaded_client function, while sending and recieving messages to and from client.")
         #     break
@@ -64,6 +65,6 @@ userID = 0
 while True:
     conn, addr = s.accept()
     print("Connected to:", addr)
-    to_send.append([])
+    # to_send.append([])
     start_new_thread(threaded_client, (conn, userID))
     userID += 1
